e21e5067364bc55afac64322cfb4de57a3ecd989
Quinten Averhals <quinten.averhals@gmail.com>
2018-02-08 15:44:07 +0100

    Added Music

--- Firstapp/app/src/main/java/com/example/casper/firstapp/FinishActivity.java
+++ Firstapp/app/src/main/java/com/example/casper/firstapp/FinishActivity.java
@@ -13,6 +13,7 @@ public class FinishActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_finish);
+        Music.doThemeSong(this);
     }
 
     @Override
---
+++ Firstapp/app/src/main/java/com/example/casper/firstapp/Music.java
@@ -0,0 +1,39 @@
+package com.example.casper.firstapp;
+
+import android.content.Context;
+import android.media.MediaPlayer;
+
+/**
+ * Created by quint on 08/02/2018.
+ */
+
+public class Music {
+    private static MediaPlayer themeSong = null;
+    private static MediaPlayer positiveCheer = null;
+
+    public static void doThemeSong(Context context) {
+        if (themeSong == null) {
+            themeSong = MediaPlayer.create(context, R.raw.dora_theme_song);
+        }
+        else if (themeSong.isPlaying()){
+            themeSong.pause();
+            themeSong.seekTo(0);
+        }
+        else {
+            themeSong.start();
+        }
+    }
+
+    public static void doPositiveCheer(Context context) {
+        if (positiveCheer == null) {
+            positiveCheer = MediaPlayer.create(context, R.raw.cheer_positive);
+        }
+        else if (positiveCheer.isPlaying()){
+            positiveCheer.pause();
+            positiveCheer.seekTo(0);
+        }
+        else {
+            positiveCheer.start();
+        }
+    }
+}
